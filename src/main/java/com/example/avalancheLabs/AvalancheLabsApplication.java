c1cfced7242c8b5583d596c12586a30b3d55c5f2
vyvajr <vyvajunior@gmail.com>
2021-06-24 21:58:19 +0300

    Trying to fix embedded Redis issue

--- src/main/java/com/example/avalancheLabs/AvalancheLabsApplication.java
+++ src/main/java/com/example/avalancheLabs/AvalancheLabsApplication.java
@@ -3,12 +3,13 @@ package com.example.avalancheLabs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("com.example.avalancheLabs.repositories")
 @EntityScan("com.example.avalancheLabs.models")
 @SpringBootApplication
-public class AvalancheLabsApplication {
+public class AvalancheLabsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AvalancheLabsApplication.class, args);
--- src/main/java/com/example/avalancheLabs/configurations/EmbededRedis.java
+++ src/main/java/com/example/avalancheLabs/configurations/EmbededRedis.java
@@ -1,17 +1,31 @@
 package com.example.avalancheLabs.configurations;
 
+import com.example.avalancheLabs.controllers.ExceptionHandlingController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import redis.embedded.RedisServer;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.io.IOException;
 
 @Component
 public class EmbededRedis {
     private RedisServer redisServer;
 
-    public EmbededRedis(RedisProperties redisProperties) {
+    //private static Logger logger =  LoggerFactory.getLogger(EmbededRedis.class);
+
+    public EmbededRedis(RedisProperties redisProperties) throws IOException {
         int port = redisProperties.getRedisPort();
-        this.redisServer = new RedisServer(port);
+        String host = redisProperties.getRedisHost();
+        //logger.info(host + "  " + port);
+
+        this.redisServer = RedisServer.builder()
+                .port(port)
+                //.redisExecProvider(customRedisExec) //com.github.kstyrc (not com.orange.redis-embedded)
+                .setting("maxmemory 128M") //maxheap 128M
+                .build();
+        // this.redisServer = new RedisServer(port);
     }
 
     @PostConstruct
